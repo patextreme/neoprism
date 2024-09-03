323d7d2a1f6c6bab6e44e639ca306d7b70da3914
Pat Losoponkul <patextreme@hotmail.com>
2024-09-03 22:02:39 +0700

    Pagination ui

--- prism-node/src/http/views/explorer.rs
+++ prism-node/src/http/views/explorer.rs
@@ -37,16 +37,18 @@ pub fn DltCursorStat(cursor: Option<DltCursor>) -> Element {
         .map(|i| HexStr::from(i.block_hash).to_string())
         .unwrap_or_else(|| "Sync is not enabled".to_string());
     rsx! {
-        div {
-            class: "stats shadow",
-            "hx-post": "{rpc_uri}",
-            "hx-vals": "{rpc}",
-            "hx-trigger": "load delay:2s",
-            "hx-swap": "outerHTML",
-            div { class: "stat",
-                div { class: "stat-title", "Current Sync Slot" }
-                div { class: "stat-value", "{slot}" }
-                div { class: "stat-desc", "{hash}" }
+        div { class: "flex flex-col items-center",
+            div {
+                class: "stats shadow",
+                "hx-post": "{rpc_uri}",
+                "hx-vals": "{rpc}",
+                "hx-trigger": "load delay:2s",
+                "hx-swap": "outerHTML",
+                div { class: "stat",
+                    div { class: "stat-title", "Current Sync Slot" }
+                    div { class: "stat-value", "{slot}" }
+                    div { class: "stat-desc", "{hash}" }
+                }
             }
         }
     }
@@ -62,18 +64,47 @@ pub fn DidList(dids: Paginated<CanonicalPrismDid>) -> Element {
         let uri = uri!(crate::http::routes::resolver(Some(did.to_string())));
         rsx! { a { class: "link font-mono", href: "{uri}", "{did}" } }
     });
+    let pagination_items = (0..dids.total_pages).map(|i: u64| {
+        let is_active = i == dids.current_page;
+        let goto_page_rpc = escape_html_rpc(&HxRpc::GetExplorerDidList { page: Some(i) });
+        let classes = if is_active {
+            format!("join-item btn btn-active")
+        } else {
+            format!("join-item btn")
+        };
+        rsx! {
+            button {
+                class: classes,
+                "hx-post": "{rpc_uri}",
+                "hx-vals": "{goto_page_rpc}",
+                "hx-target": "closest #did-list",
+                "hx-swap": "outerHTML",
+                "{i + 1}"
+            }
+        }
+    });
+    let pagination = rsx! {
+        div { class: "join my-2",
+            for i in pagination_items {
+                {i}
+            }
+        }
+    };
     rsx! {
         div {
-            class: "my-2",
+            class: "flex flex-col items-center",
+            "id": "did-list",
             "hx-post": "{rpc_uri}",
             "hx-vals": "{rpc}",
             "hx-trigger": "load delay:5s",
             "hx-swap": "outerHTML",
+            {pagination.clone()},
             ul {
                 for elem in did_elems {
                     li { {elem} }
                 }
             }
+            {pagination}
         }
     }
 }
