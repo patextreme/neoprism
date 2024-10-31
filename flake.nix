6f8ff6025b117e6de681819480c49c3187300b70
Pat Losoponkul <patextreme@hotmail.com>
2024-10-31 22:04:36 +0700

    Upgrade project dependencies (#20)

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
           config.unfree = true;
           overlays = [ (import rust-overlay) ];
         };
-        rust = pkgs.rust-bin.nightly."2024-07-26".default.override {
+        rust = pkgs.rust-bin.nightly."2024-10-30".default.override {
           extensions = [
             "rust-src"
             "rust-analyzer"
