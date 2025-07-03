e95886e1fab94b67869091ae5a14021e133ef771
Pat Losoponkul <patextreme@hotmail.com>
2025-07-03 14:14:39 +0700

    Upgrade oura to 1.9.4 (#53)
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- nix/cargo.nix
+++ nix/cargo.nix
@@ -2,7 +2,7 @@
   cargoLock = {
     lockFile = ../Cargo.lock;
     outputHashes = {
-      "oura-1.9.3" = "sha256-6AmkWenBynDyuVgWmfEGuhFiN/OK/54wWBAsp19mzyo=";
+      "oura-1.9.4" = "sha256-SaSJOlxnM2+BDg9uE4GUxKync37DJQD+P4VVZA2NO3g=";
     };
   };
 }
