63c556732ae0373f7491bf63bbc9eaa3ddbc2967
Pat Losoponkul <patextreme@hotmail.com>
2025-07-17 01:02:18 +0700

    working testnet with bootstrapping transaction
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- nix/packages/cardano-testnet-docker.nix
+++ nix/packages/cardano-testnet-docker.nix
@@ -15,17 +15,23 @@ let
           --output-dir testnet
       '';
     };
-    # transactGenesis = writeShellApplication {
-    #   name = "transactGenesis";
-    #   text = ''
-    #     TX_IN="$(cardano-cli conway query utxo --address "$(cat ./genesis/utxo-keys/shelley.000.addr)" | jq -r '. | keys[]')"
-    #     cardano-cli conway transaction build \
-    #       --tx-in "$TX_IN" \
-    #       --tx-out 'addr_test1qp83v2wq3z9mkcjj5ejlupgwt6tcly5mtmz36rpm8w4atvqd5jzpz23y8l4dwfd9l46fl2p86nmkkx5keewdevqxhlyslv99j3+100000000000' \
-    #       --change-address addr_test1qp83v2wq3z9mkcjj5ejlupgwt6tcly5mtmz36rpm8w4atvqd5jzpz23y8l4dwfd9l46fl2p86nmkkx5keewdevqxhlyslv99j3 \
-    #       --out-file tx.raw
-    #   '';
-    # };
+    transactGenesis = writeShellApplication {
+      name = "transactGenesis";
+      text = ''
+        mkdir -p transactions
+        TX_IN="$(cardano-cli conway query utxo --address "$(cat ./testnet/utxo-keys/utxo1/utxo.addr)" | jq -r '. | keys[]')"
+        cardano-cli conway transaction build \
+          --tx-in "$TX_IN" \
+          --tx-out 'addr_test1qp83v2wq3z9mkcjj5ejlupgwt6tcly5mtmz36rpm8w4atvqd5jzpz23y8l4dwfd9l46fl2p86nmkkx5keewdevqxhlyslv99j3+100000000000' \
+          --change-address addr_test1qp83v2wq3z9mkcjj5ejlupgwt6tcly5mtmz36rpm8w4atvqd5jzpz23y8l4dwfd9l46fl2p86nmkkx5keewdevqxhlyslv99j3 \
+          --out-file transactions/tx.raw
+        cardano-cli conway transaction sign \
+          --tx-body-file transactions/tx.raw \
+          --signing-key-file ./testnet/utxo-keys/utxo1/utxo.skey \
+          --out-file transactions/tx.signed
+        cardano-cli conway transaction submit --tx-file transactions/tx.signed
+      '';
+    };
   };
   basePackages = with pkgs; [
     bash
