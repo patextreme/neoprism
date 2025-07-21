7dfd3a801454213eda61fbecc245bed156bf8b7b
Pat Losoponkul <patextreme@hotmail.com>
2025-07-21 14:05:57 +0700

    add preprod docker
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- nix/devShells/default.nix
+++ nix/devShells/default.nix
@@ -7,8 +7,9 @@ let
     runtimeInputs = with pkgs; [ dhall-json ];
     text = ''
       cd "${rootDir}/docker/.config"
-      dhall-to-yaml <<< "(./main.dhall).mainnet-relay" > "${rootDir}/docker/mainnet-relay/compose.yml"
       dhall-to-yaml <<< "(./main.dhall).mainnet-dbsync" > "${rootDir}/docker/mainnet-dbsync/compose.yml"
+      dhall-to-yaml <<< "(./main.dhall).mainnet-relay" > "${rootDir}/docker/mainnet-relay/compose.yml"
+      dhall-to-yaml <<< "(./main.dhall).preprod-relay" > "${rootDir}/docker/preprod-relay/compose.yml"
       dhall-to-yaml <<< "(./main.dhall).testnet-local" > "${rootDir}/docker/testnet-local/compose.yml"
     '';
   };
