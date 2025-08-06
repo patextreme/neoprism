de4fe16979a0753ab6fff9fc102ecf37b9c87227
Pat Losoponkul <patextreme@hotmail.com>
2025-08-06 19:29:44 +0700

    more storage tests
    Signed-off-by: Pat Losoponkul <patextreme@hotmail.com>

--- tests/prism-test/src/test/scala/org/hyperledger/identus/prismtest/suite/CreateStorageOperationSuite.scala
+++ tests/prism-test/src/test/scala/org/hyperledger/identus/prismtest/suite/CreateStorageOperationSuite.scala
@@ -9,17 +9,113 @@ import zio.test.Assertion.*
 
 object CreateStorageOperationSuite extends TestUtils:
 
-  private def extractStorage(didData: DIDData): Seq[String] =
+  private def extractStorageHex(didData: DIDData): Seq[String] =
     didData.storageData
       .flatMap(_.data.bytes)
       .map(_.toByteArray().toHexString)
 
   def allSpecs = suite("CreateStorageOperation")(
-    publicKeySpec
-  ) @@ NodeName.skipIf("prism-node")
+    publicKeySpec,
+    deactivatedSpec,
+    nonceSpec
+  ) @@ NodeName.skipIf("prism-node", "scala-did")
+
+  private def deactivatedSpec = suite("Deactivated DID")(
+    test("deactivated DID cannot create a storage") {
+      for
+        seed <- newSeed
+        spo1 = builder(seed).createDid
+          .key("master-0")(KeyUsage.MASTER_KEY secp256k1 "m/0'/1'/0'")
+          .key("vdr-0")(KeyUsage.VDR_KEY secp256k1 "m/0'/8'/0'")
+          .build
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        did = spo1.getDid.get
+        spo2 = builder(seed)
+          .deactivateDid(spo1.getOperationHash.get, did)
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        spo3 = builder(seed)
+          .createStorage(did)
+          .bytes("00".decodeHex)
+          .build
+          .signWith("vdr-0", deriveSecp256k1(seed)("m/0'/8'/0'"))
+        _ <- scheduleOperations(Seq(spo1, spo2, spo3))
+        storage <- getDidDocument(did).map(_.get).map(extractStorageHex)
+      yield assert(storage)(isEmpty)
+    },
+    test("deactivated DID has all storage removed") {
+      for
+        seed <- newSeed
+        spo1 = builder(seed).createDid
+          .key("master-0")(KeyUsage.MASTER_KEY secp256k1 "m/0'/1'/0'")
+          .key("vdr-0")(KeyUsage.VDR_KEY secp256k1 "m/0'/8'/0'")
+          .build
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        did = spo1.getDid.get
+        spo2 = builder(seed)
+          .createStorage(did)
+          .bytes("00".decodeHex)
+          .build
+          .signWith("vdr-0", deriveSecp256k1(seed)("m/0'/8'/0'"))
+        spo3 = builder(seed)
+          .deactivateDid(spo2.getOperationHash.get, did)
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        _ <- scheduleOperations(Seq(spo1, spo2, spo3))
+        storage <- getDidDocument(did).map(_.get).map(extractStorageHex)
+      yield assert(storage)(isEmpty)
+    }
+  )
+
+  private def nonceSpec = suite("Nonce")(
+    test("create storage with same data and same nonce should not be indexed") {
+      for
+        seed <- newSeed
+        spo1 = builder(seed).createDid
+          .key("master-0")(KeyUsage.MASTER_KEY secp256k1 "m/0'/1'/0'")
+          .key("vdr-0")(KeyUsage.VDR_KEY secp256k1 "m/0'/8'/0'")
+          .build
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        did = spo1.getDid.get
+        spo2 = builder(seed)
+          .createStorage(did)
+          .bytes("00".decodeHex)
+          .build
+          .signWith("vdr-0", deriveSecp256k1(seed)("m/0'/8'/0'"))
+        spo3 = builder(seed)
+          .createStorage(did)
+          .bytes("00".decodeHex)
+          .build
+          .signWith("vdr-0", deriveSecp256k1(seed)("m/0'/8'/0'"))
+        _ <- scheduleOperations(Seq(spo1, spo2, spo3))
+        storage <- getDidDocument(did).map(_.get).map(extractStorageHex)
+      yield assert(storage)(hasSameElements(Seq("00")))
+    },
+    test("create storage with same data and different nonce should be indexed successfully") {
+      for
+        seed <- newSeed
+        spo1 = builder(seed).createDid
+          .key("master-0")(KeyUsage.MASTER_KEY secp256k1 "m/0'/1'/0'")
+          .key("vdr-0")(KeyUsage.VDR_KEY secp256k1 "m/0'/8'/0'")
+          .build
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        did = spo1.getDid.get
+        spo2 = builder(seed)
+          .createStorage(did, Array(0))
+          .bytes("00".decodeHex)
+          .build
+          .signWith("vdr-0", deriveSecp256k1(seed)("m/0'/8'/0'"))
+        spo3 = builder(seed)
+          .createStorage(did, Array(1))
+          .bytes("00".decodeHex)
+          .build
+          .signWith("vdr-0", deriveSecp256k1(seed)("m/0'/8'/0'"))
+        _ <- scheduleOperations(Seq(spo1, spo2, spo3))
+        storage <- getDidDocument(did).map(_.get).map(extractStorageHex)
+      yield assert(storage)(hasSameElements(Seq("00", "00")))
+    },
+  )
 
   private def publicKeySpec = suite("PublicKey")(
-    test("create storage with valid VDR key should be indexed") {
+    test("create storage with valid VDR key should be indexed successfully") {
       for
         seed <- newSeed
         spo1 = builder(seed).createDid
@@ -30,11 +126,69 @@ object CreateStorageOperationSuite extends TestUtils:
         did = spo1.getDid.get
         spo2 = builder(seed)
           .createStorage(spo1.getDid.get)
-          .bytes("001122".decodeHex)
+          .bytes("00".decodeHex)
           .build
           .signWith("vdr-0", deriveSecp256k1(seed)("m/0'/8'/0'"))
         _ <- scheduleOperations(Seq(spo1, spo2))
-        storage <- getDidDocument(did).map(_.get).map(extractStorage)
-      yield assert(storage)(hasSameElements(Seq("001122")))
-    } @@ TestAspect.tag("dev")
+        storage <- getDidDocument(did).map(_.get).map(extractStorageHex)
+      yield assert(storage)(hasSameElements(Seq("00")))
+    },
+    test("create storage with invalid VDR key should not be indexed") {
+      for
+        seed <- newSeed
+        spo1 = builder(seed).createDid
+          .key("master-0")(KeyUsage.MASTER_KEY secp256k1 "m/0'/1'/0'")
+          .key("vdr-0")(KeyUsage.VDR_KEY secp256k1 "m/0'/8'/0'")
+          .build
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        did = spo1.getDid.get
+        spo2 = builder(seed)
+          .createStorage(spo1.getDid.get)
+          .bytes("00".decodeHex)
+          .build
+          .signWith("vdr-0", deriveSecp256k1(seed)("m/0'/8'/1'"))
+        _ <- scheduleOperations(Seq(spo1, spo2))
+        storage <- getDidDocument(did).map(_.get).map(extractStorageHex)
+      yield assert(storage)(isEmpty)
+    },
+    test("create storage with non-VDR key should not be indexed") {
+      for
+        seed <- newSeed
+        spo1 = builder(seed).createDid
+          .key("master-0")(KeyUsage.MASTER_KEY secp256k1 "m/0'/1'/0'")
+          .build
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        did = spo1.getDid.get
+        spo2 = builder(seed)
+          .createStorage(spo1.getDid.get)
+          .bytes("00".decodeHex)
+          .build
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        _ <- scheduleOperations(Seq(spo1, spo2))
+        storage <- getDidDocument(did).map(_.get).map(extractStorageHex)
+      yield assert(storage)(isEmpty)
+    },
+    test("create storage with removed VDR key should not be indexed") {
+      for
+        seed <- newSeed
+        spo1 = builder(seed).createDid
+          .key("master-0")(KeyUsage.MASTER_KEY secp256k1 "m/0'/1'/0'")
+          .key("vdr-0")(KeyUsage.MASTER_KEY secp256k1 "m/0'/8'/0'")
+          .build
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        did = spo1.getDid.get
+        spo2 = builder(seed)
+          .updateDid(spo1.getOperationHash.get, did)
+          .removeKey("vdr-0")
+          .build
+          .signWith("master-0", deriveSecp256k1(seed)("m/0'/1'/0'"))
+        spo3 = builder(seed)
+          .createStorage(did)
+          .bytes("00".decodeHex)
+          .build
+          .signWith("vdr-0", deriveSecp256k1(seed)("m/0'/8'/0'"))
+        _ <- scheduleOperations(Seq(spo1, spo2, spo3))
+        storage <- getDidDocument(did).map(_.get).map(extractStorageHex)
+      yield assert(storage)(isEmpty)
+    }
   )
